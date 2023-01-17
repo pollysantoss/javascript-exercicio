d962f2764efa7b88b4af50f7651d1f6e95ee8586
Pollyanna Teixeira Santos <pollyteisantos@gmail.com>
2023-01-17 01:18:50 -0300

    feat(questao5): Add condicionais pra divisíveis por 3 e 5 separadas

--- questao5.js
+++ questao5.js
@@ -4,3 +4,13 @@ num = parseInt(num)
 if(((num % 3) == 0) && ((num % 5) == 0)){
     console.log("fizzbuzz")
 }
+
+else
+    if((num % 3) == 0){
+    console.log("fizz")
+    }
+
+    else
+        if((num % 5) == 0){
+            console.log("buzz")
+        }
