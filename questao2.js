db6461ee2c91e131fee38722ebb7baf5c980f087
Pollyanna Teixeira Santos <pollyteisantos@gmail.com>
2023-01-17 00:57:23 -0300

    feat(questao2):Add console.log

--- questao2.js
+++ questao2.js
@@ -2,3 +2,5 @@ var fahrenheit = prompt("Digite uma temperatura em Fahrenheit:")
 fahrenheit = parseFloat(fahrenheit)
 
 var celsius = 5 * ((fahrenheit - 32)/9)
+
+console.log("O valor correspondente a " + fahrenheit + "°F em Celsius é " + celsius + "°C.")
