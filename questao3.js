f147d4637e6033cf270e99223792108a3e0c181d
Pollyanna Teixeira Santos <pollyteisantos@gmail.com>
2023-01-18 01:14:08 -0300

    feat(questao3): Add de varriaveis num e parsefloat nessas variaveis

--- questao3.js
+++ questao3.js
@@ -1,1 +1,7 @@
-q
+var nota1 = prompt("Digite nota 1 do aluno:")
+var nota2 = prompt("Digite nota 2 do aluno:")
+var nota3 = prompt("Digite nota 3 do aluno:")
+
+nota1 = parseFloat(nota1)
+nota2 = parseFloat(nota2)
+nota3 = parseFloat(nota3)
