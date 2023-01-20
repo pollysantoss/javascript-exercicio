2a3bf0b193aa751185e97983fe6afb00004c73e6
Pollyanna Teixeira Santos <pollyteisantos@gmail.com>
2023-01-20 18:40:25 -0300

    feat(extra):Add ex 2 extra

--- exercicios-extras/ex2.js
+++ exercicios-extras/ex2.js
@@ -1,13 +1,14 @@
+var numX = prompt("Digite o primeiro número:")
+var numN = prompt("Digite o segundo número:")
+numX = parseInt(num1)
+numN = parseInt(num2)
 
-function inputNum(num){
-    var num1 = prompt("Digite o primeiro número:")
-    var num2 = prompt("Digite o segundo número:")
-    num1 = parseInt(num1)
-    num2 = parseInt(num2)
-    
+function multNum (num1,num2){
+  var contador = num2
+  if(contador > 0) { 
+  num1 += num1
+  }
+  return (multNum(num1,(num2 - 1)))
 }
 
-function mult(inputNum){
-    var mul= num1 * num2
-    callback(mul)
-}
+alert(numX + "x" + numN + "=" multNum(numX,numN))
