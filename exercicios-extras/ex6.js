92d3359f8ef99adc5a2f58cf91a2e90b9e280b47
Pollyanna Teixeira Santos <pollyteisantos@gmail.com>
2023-01-20 23:06:11 -0300

    feat(extra):Atualização questao 6 extra

--- exercicios-extras/ex6.js
+++ exercicios-extras/ex6.js
@@ -3,34 +3,32 @@ var valores = {
     despesas: [],
   }
   
-  valores[0] = prompt("Digite os valores da sua receita separado por vírgula:").split(",")
-  valores[1] = prompt("Digite o valor das suas despesas separado por vírgula:").split(",")
+receitas = prompt("Digite os valores da sua receita separado por vírgula:").split(",")
+despesas = prompt("Digite o valor das suas despesas separado por vírgula:").split(",")
   
-  valores[0] = parseFloat(valores[0])
-  valores[1] = parseFloat(valores[1])
+receitas = parseFloat(receitas)
+despesas = parseFloat(despesas)
   
-  function saldo(num1, num2){
+function saldo(receitas, despesas){
     var totalReceitas = 0
-    for(var i = 0; i <num1.length; i++){
-        totalReceitas += num1[i]
+    for(var i = 0; i  <receitas.length; i++){
+        totalReceitas += receitas[i]
     }
   
     var totalDespesas = 0
-    for(var i = 0; i < num2.length; i++){
-        totalDespesas += num2[i]
+    for(var i = 0; i < despesas.length; i++){
+        totalDespesas += despesas[i]
     }
   
     var saldoTotal = totalReceitas - totalDespesas
-  
-    if(saldoTotal < 0){
-        return "Saldo negativo, valor do saldo = " + saldoTotal
-    }
-  
-    else{
-        return "Saldo positivo, valor do saldo = " + saldoTotal 
-    }
-  
+    return saldoTotal
   }
   
-  saldo(valores[0],valores[1])
+  
+if((saldo(receitas,despesas)) < 0){
+    alert("Saldo negativo, valor do saldo = " + saldo(receitas,despesas))
+}
+  else{
+    alert("Saldo positivo, valor do saldo = " + saldo(receitas,despesas))
+}
 
