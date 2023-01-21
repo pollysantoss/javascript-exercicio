cb7e434436eaf7eb9fdb6d74b364d9ae557b31da
Pollyanna Teixeira Santos <pollyteisantos@gmail.com>
2023-01-21 11:36:08 -0300

    feat(questao1): Questao 1 matrizes feita

--- questao1.js
+++ questao1.js
@@ -1,38 +1,22 @@
-var matriz1 = []
-var matriz2 = []
-var linha1matriz1 = []
-var linha2matriz1 = []
-var linha1matriz2 = []
-var linha2matriz2 = []
-
-for (var contador = 0; contador == 1; contador++) {
-    linha1matriz1 = (prompt("Digite os 2 números da primeira linha da matriz 2x2, separados por vírgula")).split(",")
-    linha2matriz1 = (prompt("Digite os 2 números da segunda linha da matriz 2x2 separados por vírgula")).split(",")
-    linha1matriz1 = parseFloat(linha1matriz1)
-    linha2matriz1 = parseFloat(linha2matriz1)
+var matriz1 = [[4,0],[-1,-1]]
+var matriz2 = [[-1,3],[2,7]]
+for(var i; i < matriz1.length; i++){
+  matriz1[i] = parseFloat(matriz1[i])
 }
-
-matriz1.push(linha1matriz1,linha2matriz1)
-
-for (var contador = 0; contador == 1; contador++) {
-  linha1matriz2 = (prompt("Digite os 2 números da primeira linha da matriz 2x2, separados por vírgula")).split(",")
-  linha2matriz2 = (prompt("Digite os 2 números da segunda linha da matriz 2x2 separados por vírgula")).split(",")
-  linha1matriz2 = parseFloat(linha1matriz2)
-  linha2matriz2 = parseFloat(linha2matriz2)
+for(var i; i < matriz2.length; i++){
+  matriz2[i] = parseFloat(matriz2[i])
 }
-
-matriz2.push(linha1matriz2,linha2matriz2)
 
 function multMatriz(matriz1,matriz2){
-  var linha1_final = [[((matriz1[0][0] * matriz2[0][0]) + (matriz1[0][1] * matriz2[0][1]))],
-  [((matriz1[0][0] * matriz2[0][1]) + (matriz1[0][1] * matriz2[0][0]))]]
+  var linha1_final = [[((matriz1[0][0] * matriz2[0][0]) + (matriz1[0][1] * matriz2[1][0]))],
+  [((matriz1[0][0] * matriz2[0][1]) + (matriz1[0][1] * matriz2[1][1]))]]
 
-  var linha2_final = [[((matriz1[1][0] * matriz2[1][1]) + (matriz1[1][1] * matriz2[1][0]))],
-  [((matriz1[1][0] * matriz2[1][0]) + (matriz1[1][1] * matriz2[1][1]))]]
+  var linha2_final = [[((matriz1[1][0] * matriz2[0][0]) + (matriz1[1][1] * matriz2[1][0]))],
+  [((matriz1[1][0] * matriz2[0][1]) + (matriz1[1][1] * matriz2[1][1]))]]
 
-  var matrizFinal = matrizFinal.push(linha1_final,linha2_final)
+  var matrizFinal = [[linha1_final],[linha2_final]]
+
   return matrizFinal
 }
 
 console.log("O resultado da multiplicação das matrizes é :" + multMatriz(matriz1,matriz2))
-
